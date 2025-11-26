09e4be374e6229e4bb6de10644f86a0d62b6e89c
supremeamerapplication <supremeamerapplication@gmail.com>
2025-11-26 18:56:51 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'imostar7-portfolio-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/privacy.html',
+  '/terms.html',
+  'IMG-20250901-WA0014.jpg',
+  'IMG-20251123-WA0016.jpg'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
